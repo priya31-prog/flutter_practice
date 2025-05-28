e6f850b94d74a331f2399cbf0bb05ba424d44135
priya31-prog <karthika200103@gmail.com>
2025-05-28 21:04:50 +0530

    changes in enddrawer using list tile

--- isolates_mixins/lib/mixins/left_drawer_menu.dart
+++ isolates_mixins/lib/mixins/left_drawer_menu.dart
@@ -2,53 +2,89 @@ import 'package:flutter/material.dart';
 
 mixin DrawerMixin {
   Widget drawerWidget(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
-        Container(
-          padding: EdgeInsets.all(15),
-          // alignment: Alignment.bottomRight,
-          color: Theme.of(context).primaryColor.withAlpha(40),
-          child: textIconButton(
-            icon: Icons.menu_book_outlined,
-            text: 'Example 1',
-            iconBtnPress: () {
-              Navigator.pop(context);
-            },
+        DrawerHeader(
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor.withAlpha(40),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(15.0),
-          //convert this column into a listview
-          child: Column(
-            spacing: 20,
-            children: [
-              // SizedBox(height: 20),
-              textIconButton(
-                icon: Icons.access_alarm_outlined,
-                text: 'Hero widget',
-              ),
-              // SizedBox(height: 20),
-              textIconButton(
-                icon: Icons.access_alarm_outlined,
-                text: 'Grid View',
-              ),
-              // SizedBox(height: 20),
-              textIconButton(
-                icon: Icons.access_alarm_outlined,
-                text: 'Animations',
-              ),
-              // SizedBox(height: 20),
-              textIconButton(
-                icon: Icons.access_alarm_outlined,
-                text: 'List view',
-              ),
-            ],
+          child: textIconButton(
+            icon: Icons.menu,
+            text: 'Menu Bar',
+            iconBtnPress: () => Navigator.pop(context),
           ),
         ),
 
-        // IconButton(onPressed: () {}, icon: Icon(Icons.one_k_plus_outlined)),
+        ListTile(
+          title: textIconButton(icon: Icons.onetwothree, text: 'Hero Widget'),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(left: 25, top: 15),
+            child: Column(
+              spacing: 10,
+              children: [
+                textIconButton(
+                  icon: Icons.slideshow_rounded,
+                  text: 'Slider',
+                  iconSize: 15,
+                ),
+                textIconButton(
+                  icon: Icons.pages_outlined,
+                  text: 'Pagination',
+                  iconSize: 15,
+                ),
+              ],
+            ),
+          ),
+        ),
+        ListTile(
+          title: textIconButton(icon: Icons.onetwothree, text: 'Grid View'),
+        ),
+        ListTile(
+          title: textIconButton(
+            icon: Icons.onetwothree,
+            text: 'Layout builder',
+          ),
+        ),
+        ListTile(
+          title: textIconButton(icon: Icons.onetwothree, text: 'Animations'),
+        ),
       ],
     );
+
+    // Column(
+    //   mainAxisSize: MainAxisSize.min,
+    //   crossAxisAlignment: CrossAxisAlignment.center,
+    //   children: [
+    //     Container(
+    //       padding: EdgeInsets.all(15),
+    //       // alignment: Alignment.bottomRight,
+    //       color: Theme.of(context).primaryColor.withAlpha(40),
+    //       child: textIconButton(
+    //         icon: Icons.menu_book_outlined,
+    //         text: 'Example 1',
+    //         iconBtnPress: () {
+    //           Navigator.pop(context);
+    //         },
+    //       ),
+    //     ),
+
+    //     Column(children: [Container()]),
+    //     // Container(
+    //     //   height: 400,
+    //     //   child: ListView.builder(
+    //     //     itemBuilder: (BuildContext context, int index) {
+    //     //       return textIconButton(
+    //     //         icon: Icons.access_alarm,
+    //     //         text: 'Hero Widget',
+    //     //       );
+    //     //     },
+    //     //     itemCount: 4,
+    //     //   ),
+    //     // ),
+
+    //     // IconButton(onPressed: () {}, icon: Icon(Icons.one_k_plus_outlined)),
+    //   ],
+    // );
   }
 }
 
@@ -57,11 +93,29 @@ Widget textIconButton({
   required IconData icon,
   required String text,
   TextStyle? textStyle,
+  double? iconSize,
 }) {
   return InkWell(
     onTap: iconBtnPress,
     child: Row(
-      children: [Icon(icon), SizedBox(width: 8), Text(text, style: textStyle)],
+      children: [
+        Icon(icon, size: iconSize),
+        SizedBox(width: 8),
+        Text(text, style: textStyle),
+      ],
     ),
   );
 }
+
+// Widget textIconButton({
+//   required IconData icon,
+//   required String text,
+//   VoidCallback? iconBtnPress,
+//   TextStyle? style,
+// }) {
+//   return TextButton.icon(
+//     onPressed: iconBtnPress, // Provide an onPressed callback
+//     icon: Icon(icon),
+//     label: Text(text, style: style),
+//   );
+// }
