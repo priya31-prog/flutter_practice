de388023696d0db7f49379f057cd38290d04933f
priya31-prog <karthika200103@gmail.com>
2025-05-29 08:49:05 +0530

    state management

--- isolates_mixins/lib/main.dart
+++ isolates_mixins/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:isolates_mixins/common_files/route_navigation.dart';
 import 'package:isolates_mixins/mixins/mixin_main.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
---
+++ isolates_mixins/lib/state_management/state_provider.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final expandProvider = StateProvider<bool>((ref) => false);
