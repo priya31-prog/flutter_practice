50b0db17c1cd7fb2da3e89b8348b52fbc1ce5f3e
priya31-prog <karthika200103@gmail.com>
2025-06-09 08:32:03 +0530

    splash screen + widget test

---
+++ basic_ecommerce_app/lib/screens/splash_screen.dart
@@ -0,0 +1,69 @@
+import 'package:basic_ecommerce_app/common_files/gradient_theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<SplashScreen> {
+  bool isLoading = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 10,
+          children: [
+            ShaderMask(
+              // to give the gradient color to the icon
+              shaderCallback: (Rect bounds) {
+                return Theme.of(context)
+                    .extension<GradientTheme>()!
+                    .primaryGradient
+                    .createShader(bounds);
+              },
+              blendMode: BlendMode.srcIn,
+              child: Icon(
+                Icons.shopping_cart_outlined,
+                size: MediaQuery.of(context).size.height * 0.25,
+                color: Colors.white,
+              ),
+            ),
+
+            Text(
+              'ElectroCart',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+            ),
+            Text(
+              'Shop the Future',
+              style: TextStyle(fontSize: 15, color: Colors.white24),
+            ),
+
+            SizedBox(height: 60),
+            isLoading
+                ? Padding(
+                  padding: EdgeInsets.all(8),
+                  child: ShaderMask(
+                    shaderCallback: (Rect bounds) {
+                      return Theme.of(context)
+                          .extension<GradientTheme>()!
+                          .primaryGradient
+                          .createShader(bounds);
+                    },
+                    child: CircularProgressIndicator(
+                      strokeWidth: 2,
+                      color: Colors.cyan,
+                    ),
+                  ),
+                )
+                : SizedBox.shrink(),
+          ],
+        ),
+      ),
+    );
+  }
+}
