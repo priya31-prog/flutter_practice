d447080b398e750059eb9f9438cb77348b0149ac
priya31-prog <karthika200103@gmail.com>
2025-06-10 22:28:27 +0530

    view - login screen

--- basic_ecommerce_app/lib/screens/login_screen.dart
+++ basic_ecommerce_app/lib/screens/login_screen.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late TextEditingController _emailController;
+  late TextEditingController _passwordController;
+  @override
+  void initState() {
+    super.initState();
+
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +40,7 @@ class LoginScreen extends StatelessWidget {
             // SizedBox(height: 20),
             Text('Login or Sign Up', style: TextStyle(fontSize: 30)),
             TextField(
+              controller: _emailController,
               decoration: InputDecoration(
                 hintText: 'Email',
                 fillColor: Theme.of(
@@ -37,6 +55,7 @@ class LoginScreen extends StatelessWidget {
             ),
 
             TextField(
+              controller: _passwordController,
               decoration: InputDecoration(
                 hintText: 'Password',
                 fillColor: Theme.of(
@@ -48,6 +67,29 @@ class LoginScreen extends StatelessWidget {
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
+            ),
+            InkWell(
+              child: Text(
+                'Forgot Password ?',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.secondary.withAlpha(250),
+                ),
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Don't have an account? "),
+            InkWell(
+              onTap: () {},
+              child: Text('Sign Up', style: TextStyle(color: Colors.cyan)),
             ),
           ],
         ),
