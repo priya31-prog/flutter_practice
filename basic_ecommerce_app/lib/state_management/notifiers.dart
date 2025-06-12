0f95ea6dd153067670a0a8692e086b9fe69560be
priya31-prog <karthika200103@gmail.com>
2025-06-12 21:05:42 +0530

    brands , gradgets list notifiers

--- basic_ecommerce_app/lib/state_management/notifiers.dart
+++ basic_ecommerce_app/lib/state_management/notifiers.dart
@@ -1,6 +1,13 @@
+import 'package:basic_ecommerce_app/api%20files/gadgets_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final isDataLoaded = StateProvider<bool>((ref) => true);
+
+final gadgetsList = StateProvider<GadgetsModel>(
+  (ref) => GadgetsModel(status: 'error', message: '', products: []),
+);
+
+final brandsList = StateProvider<List<String>>((ref) => []);
 final isCredEntered = StateProvider<bool>((ref) => false);
 final email = StateProvider<String>((ref) => '');
 final phone = StateProvider<String>((ref) => '');
