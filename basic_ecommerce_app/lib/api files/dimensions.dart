3e718d08cc04ee02d969e551b817e8a71b7540c7
priya31-prog <karthika200103@gmail.com>
2025-06-13 22:34:29 +0530

    dimension changes

--- basic_ecommerce_app/lib/api files/dimensions.dart
+++ basic_ecommerce_app/lib/api files/dimensions.dart
@@ -1,14 +1,14 @@
 class Dimensions {
-  final double? width;
-  final double? height;
-  final double? depth;
-  Dimensions({this.depth, this.height, this.width});
+  final double width;
+  final double height;
+  final double depth;
+  Dimensions({required this.depth, required this.height, required this.width});
 
   factory Dimensions.fromJson(Map<String, dynamic> json) {
     return Dimensions(
-      depth: json['depth'] ?? 0.0,
-      height: json['height'] ?? 0.0,
-      width: json['width'] ?? 0.0,
+      depth: (json['depth'] as num).toDouble(),
+      width: (json['width'] as num).toDouble(),
+      height: (json['height'] as num).toDouble(),
     );
   }
 
