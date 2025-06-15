9ee0fdadb6db5ae370e0e655c77de6e072dd2cc5
priya31-prog <karthika200103@gmail.com>
2025-06-15 18:33:56 +0530

    bottom navigator add cart

---
+++ basic_ecommerce_app/lib/screens/widgets/bottom_navigator.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+Widget bottomNavigator() {
+  return SizedBox(
+    height: 60,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      // crossAxisAlignment: CrossAxisAlignment.ce,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Expanded(
+          // flex: ,
+          child: Container(
+            // height: 60,
+            color: Colors.teal,
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsets.all(3),
+              child: IconButton(
+                alignment: Alignment.center,
+                onPressed: () {},
+                icon: Row(
+                  spacing: 5,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.add),
+                    Text(
+                      'Buy now',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            alignment: Alignment.center,
+            color: const Color.fromARGB(255, 123, 121, 121),
+            child: Padding(
+              padding: EdgeInsets.all(3),
+              child: IconButton(
+                alignment: Alignment.center,
+                onPressed: () {},
+                icon: Row(
+                  spacing: 5,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.shopping_cart),
+                    Text(
+                      'Add to Cart',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
