335cce4189c9fd5dca726fbbf0ca4d11c6390b58
priya31-prog <karthika200103@gmail.com>
2025-06-16 21:01:36 +0530

    add to cart page

---
+++ basic_ecommerce_app/lib/screens/add_to_cart_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddToCart extends StatelessWidget {
+  const AddToCart({super.key});
+
+  // final Products
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
