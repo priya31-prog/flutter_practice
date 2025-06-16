7541889b73fc766cdffb45fa88817ab523c05390
priya31-prog <karthika200103@gmail.com>
2025-06-16 08:22:00 +0530

    discount calculations

--- basic_ecommerce_app/lib/common_files/common_utils.dart
+++ basic_ecommerce_app/lib/common_files/common_utils.dart
@@ -2,4 +2,12 @@ class CommonUtils {
   static String firstLetterCaps({required String name}) {
     return (name[0].toUpperCase() + name.substring(1));
   }
+
+  static String discountedPrice({
+    required double price,
+    required double discount,
+  }) {
+    var new_price = price - ((price * discount) / 100);
+    return new_price.toStringAsFixed(2);
+  }
 }
