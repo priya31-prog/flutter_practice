816e6a708d76852afa6cf2988d06a0987a9262c4
priya31-prog <karthika200103@gmail.com>
2025-06-21 16:09:25 +0530

    cart items full model

--- basic_ecommerce_app/lib/api files/models/cart_items_model.dart
+++ basic_ecommerce_app/lib/api files/models/cart_items_model.dart
@@ -1,1 +1,27 @@
+import 'package:basic_ecommerce_app/api%20files/models/cart_products.dart';
 
+class CartItemsModel {
+  final String status;
+  final int statusCode;
+  final List<CartProducts>? cartProducts;
+  CartItemsModel({
+    required this.status,
+    required this.statusCode,
+    this.cartProducts,
+  });
+
+  factory CartItemsModel.fromJson(Map<String, dynamic> json) {
+    return CartItemsModel(
+      status: json['status'],
+      statusCode: json['success_code'],
+      cartProducts:
+          (json['data'] as List?)
+              ?.map((item) => CartProducts.fromJson(item))
+              .toList(),
+    );
+  }
+}
+
+//  (json['data'] as List?)
+//           ?.map((item) => CartItem.fromJson(item))
+//           .toList(),
