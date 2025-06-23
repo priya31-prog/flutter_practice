391902a55c4de41eecfd87e30d65f58d3a63b064
priya31-prog <karthika200103@gmail.com>
2025-06-23 21:27:08 +0530

    api header file

---
+++ basic_ecommerce_app/lib/common_files/api_header.dart
@@ -0,0 +1,17 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<Map<String, String>> getHeaders({bool withAuth = false}) async {
+  final headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  };
+
+  if (withAuth) {
+    // Fetch token from local storage
+    final preference = await SharedPreferences.getInstance();
+    final token = preference.getString('token') ?? '';
+    headers['Authorization'] = 'Token $token'; // Or Bearer
+  }
+
+  return headers;
+}
