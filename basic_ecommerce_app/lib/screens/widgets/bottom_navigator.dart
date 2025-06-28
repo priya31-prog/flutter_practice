639edd43b1b42f740cf9fe84e09a7958eef86091
priya31-prog <karthika200103@gmail.com>
2025-06-28 10:46:40 +0530

    buy now , go to cart, add to cart logics

--- basic_ecommerce_app/lib/screens/widgets/bottom_navigator.dart
+++ basic_ecommerce_app/lib/screens/widgets/bottom_navigator.dart
@@ -9,112 +9,137 @@ Widget bottomNavigator({
   required BuildContext context,
   required Products products,
   required bool isApiLoaded,
-  required bool addToCartOrGo,
+  required bool goToCart,
 }) {
   return SizedBox(
     height: 60,
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      // crossAxisAlignment: CrossAxisAlignment.ce,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Expanded(
-          // flex: ,
-          child: Container(
-            // height: 60,
-            color: Colors.teal,
-            alignment: Alignment.center,
-            child: Padding(
-              padding: EdgeInsets.all(3),
-              child: IconButton(
-                alignment: Alignment.center,
-                onPressed: () {},
-                icon: Row(
-                  spacing: 5,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.add),
-                    Text(
-                      'Buy now',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 20,
+    child:
+        isApiLoaded
+            ? Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              // crossAxisAlignment: CrossAxisAlignment.ce,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
+                  // flex: ,
+                  child: Container(
+                    // height: 60,
+                    color: Colors.teal,
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: IconButton(
+                        alignment: Alignment.center,
+                        onPressed: () {
+                          addToCartFn(
+                            context: context,
+                            products: products,
+                            goToCart: goToCart,
+                          );
+                        },
+                        icon: Row(
+                          spacing: 5,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.add),
+                            Text(
+                              'Buy now',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ],
+                  ),
                 ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    color: const Color.fromARGB(255, 123, 121, 121),
+                    child: Padding(
+                      padding: EdgeInsets.all(3),
+                      child: IconButton(
+                        alignment: Alignment.center,
+                        onPressed: () async {
+                          addToCartFn(
+                            context: context,
+                            products: products,
+                            goToCart: goToCart,
+                          );
+                        },
+                        icon: Row(
+                          spacing: 5,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.shopping_cart),
+                            goToCart
+                                ? Text(
+                                  'Go to Cart',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 20,
+                                  ),
+                                )
+                                : Text(
+                                  'Add to Cart',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+            : Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: CircularProgressIndicator(color: Colors.teal),
               ),
             ),
-          ),
-        ),
-        Expanded(
-          child: Container(
-            alignment: Alignment.center,
-            color: const Color.fromARGB(255, 123, 121, 121),
-            child: Padding(
-              padding: EdgeInsets.all(3),
-              child: IconButton(
-                alignment: Alignment.center,
-                onPressed: () async {
-                  final discount = products.discountPercent.toString();
-                  final price = products.price.toString();
-                  await AddCartItem()
-                      .addItem(
-                        product: CartProducts(
-                          addedToCartAt: DateTime.now(),
-                          shippingInfo: DateTime.now(),
-                          brand: products.brand,
-                          name: products.title,
-                          discountPercent: discount,
-                          productId: products.id,
-                          isAvailable:
-                              products.availabiltySts == 'In Stock'
-                                  ? true
-                                  : false,
-                          price: price,
-                          stock: products.stock,
-                          warrentyInfo: products.warrentyInfo,
-                          imageUrl: products.thumbnail,
-                        ),
-                      )
-                      .then((final value) {
-                        // if success show a  toast msg of added to cart successfully
-                        //if buy now -> go to cart with the newly added item and the exisiting item
-                        // re
-                        if (!context.mounted) return;
-                        Navigator.pushNamed(
-                          context,
-                          RouteNavigations.addToCartPageSkip,
-                        );
-                      });
-                },
-                icon: Row(
-                  spacing: 5,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.shopping_cart),
-                    addToCartOrGo
-                        ? Text(
-                          'Go to Cart',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w700,
-                            fontSize: 20,
-                          ),
-                        )
-                        : Text(
-                          'Add to Cart',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w700,
-                            fontSize: 20,
-                          ),
-                        ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    ),
   );
+}
+
+Future<void> addToCartFn({
+  required Products products,
+  required BuildContext context,
+  required bool goToCart,
+}) async {
+  final discount = products.discountPercent.toString();
+  final price = products.price.toString();
+
+  if (!goToCart) {
+    await AddCartItem()
+        .addItem(
+          product: CartProducts(
+            addedToCartAt: DateTime.now(),
+            shippingInfo: DateTime.now(),
+            brand: products.brand,
+            name: products.title,
+            discountPercent: discount,
+            productId: products.id,
+            isAvailable: products.availabiltySts == 'In Stock' ? true : false,
+            price: price,
+            stock: products.stock,
+            warrentyInfo: products.warrentyInfo,
+            imageUrl: products.thumbnail,
+          ),
+        )
+        .then((final value) {
+          // if success show a  toast msg of added to cart successfully
+          //if buy now -> go to cart with the newly added item and the exisiting item
+          // re
+          if (!context.mounted) return;
+          Navigator.pushNamed(context, RouteNavigations.addToCartPageSkip);
+        });
+  } else {
+    print('printing inside the go to cart page..');
+  }
 }
