0eb0cdf41aad4b2c309495f5c814af2f8bbe51fe
priya31-prog <karthika200103@gmail.com>
2025-07-03 21:49:54 +0530

    cart add/ remove

--- basic_ecommerce_app/lib/api files/cart_notifiers.dart
+++ basic_ecommerce_app/lib/api files/cart_notifiers.dart
@@ -1,35 +1,48 @@
+import 'dart:developer';
+
 import 'package:basic_ecommerce_app/api%20files/api_call.dart';
 import 'package:basic_ecommerce_app/api%20files/models/cart_products.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CartNotifier extends StateNotifier<List<CartProducts>> {
-  CartNotifier() : super([]);
+class CartNotifier extends StateNotifier<AsyncValue<List<CartProducts>>> {
+  CartNotifier() : super(const AsyncValue.loading()) {
+    loadCartItems();
+  }
 
   // API 1: Load all cart products
   Future<void> loadCartItems() async {
-    final items = await GadgetsApi().getCartItems();
-    state = items.cartProducts ?? [];
+    log('printing inside cart itesm');
+    try {
+      final items =
+          await GadgetsApi().getCartItems(); // Your API for GET /cart/items
+      state = AsyncValue.data(items.cartProducts!);
+    } catch (e, st) {
+      state = AsyncValue.error(e, st);
+    }
   }
 
-  // API 2: Add/increment item and update only the changed item in UI
-  Future<void> addCartItem(CartProducts itemToAdd) async {
-    final updatedItem = await AddCartItem().addItem(product: itemToAdd);
-
-    final index = state.indexWhere((e) => e.productId == updatedItem.productId);
-
-    if (index != -1) {
-      // Replace only that item with updated quantity
-      final updatedList = [...state];
-      updatedList[index] = updatedItem;
-      state = List<CartProducts>.from(updatedList);
-    } else {
-      // First time item added
-      state = [...state, updatedItem];
+  Future<void> incrementItem(CartProducts products) async {
+    final response = await AddCartItem().addItem(
+      product: products,
+      action: 'increment',
+    );
+    if (response.productId != null) {
+      await loadCartItems(); // re-fetch updated list
     }
-    print("✅ state updated: ${state.map((e) => e.quantity)}");
+  }
+
+  Future<void> decrementItem(CartProducts products) async {
+    final response = await AddCartItem().addItem(
+      product: products,
+      action: 'decrement',
+    );
+    if (response.productId != null) {
+      await loadCartItems(); // re-fetch updated list
+    }
   }
 }
 
-final cartProvider = StateNotifierProvider<CartNotifier, List<CartProducts>>(
-  (ref) => CartNotifier(),
-);
+final cartProvider =
+    StateNotifierProvider<CartNotifier, AsyncValue<List<CartProducts>>>(
+      (ref) => CartNotifier(),
+    );
