53e6bad86d9d0d1d66352c7dbc422e397cc06a41
priya31-prog <karthika200103@gmail.com>
2025-08-07 07:46:15 +0530

    disable conditions

--- basic_ecommerce_app/lib/screens/widgets/elevated_button_wider_button.dart
+++ basic_ecommerce_app/lib/screens/widgets/elevated_button_wider_button.dart
@@ -6,22 +6,41 @@ Widget elevatedButtonWider({
   EdgeInsetsGeometry? padding,
   Color? backgroundColor,
   TextStyle? textStyle,
+  bool? isDisabled,
 }) {
   return InkWell(
-    onTap: onTap,
-    child: Container(
-      padding: padding ?? EdgeInsets.all(13),
-      decoration: BoxDecoration(
-        color: backgroundColor ?? Colors.cyan,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Text(
-        text,
-        textAlign: TextAlign.center,
-        style:
-            textStyle ??
-            TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+    onTap: () {
+      if (isDisabled ?? false) {
+      } else {
+        onTap;
+      }
+    },
+    child: Opacity(
+      opacity: getBgColorOpacity(isDisabled: isDisabled ?? false),
+      child: Container(
+        padding: padding ?? EdgeInsets.all(13),
+        decoration: BoxDecoration(
+          color: backgroundColor ?? Colors.cyan,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style:
+              textStyle ??
+              TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+        ),
       ),
     ),
   );
+}
+
+double getBgColorOpacity({
+  // required Color backgroundColor,
+  required bool isDisabled,
+}) {
+  if (isDisabled) {
+    return 0.3;
+  }
+  return 1;
 }
