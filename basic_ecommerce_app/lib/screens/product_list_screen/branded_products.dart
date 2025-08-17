cb2d91535a1a11be3ed0dbaffe27789a90f6a0b9
priya31-prog <karthika200103@gmail.com>
2025-08-17 21:36:06 +0530

    branded products screen

---
+++ basic_ecommerce_app/lib/screens/product_list_screen/branded_products.dart
@@ -0,0 +1,54 @@
+import 'package:basic_ecommerce_app/api%20files/models/products_model.dart';
+import 'package:basic_ecommerce_app/screens/product_list_screen/grid_view_products.dart';
+import 'package:flutter/material.dart';
+
+class BrandedProducts extends StatelessWidget {
+  const BrandedProducts({super.key, required this.brandedProductsArgs});
+
+  final BrandedProductsArgs brandedProductsArgs;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsetsGeometry.all(10),
+                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.cyan),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  spacing: 10,
+                  children: [
+                    Text(
+                      brandedProductsArgs.brandedProducts[0].brand,
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                    Icon(Icons.close, weight: 1),
+                  ],
+                ),
+              ),
+              getAllProducts(
+                context: context,
+                products: brandedProductsArgs.brandedProducts,
+                ontap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BrandedProductsArgs {
+  final List<Products> brandedProducts;
+  BrandedProductsArgs({required this.brandedProducts});
+}
