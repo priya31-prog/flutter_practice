619fea629f5017acd0207899a79caac329c80cd1
priya31-prog <karthika200103@gmail.com>
2025-08-25 14:29:42 +0530

    text form field validation

--- basic_ecommerce_app/lib/screens/login_profile/sign_up_screen.dart
+++ basic_ecommerce_app/lib/screens/login_profile/sign_up_screen.dart
@@ -5,22 +5,23 @@ import 'dart:developer';
 import 'package:basic_ecommerce_app/common_files/route_navigations.dart';
 import 'package:basic_ecommerce_app/screens/login_profile/auth_service.dart';
 import 'package:basic_ecommerce_app/screens/login_profile/user_info_model.dart';
-import 'package:basic_ecommerce_app/state_management/notifiers.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SignUpScreen extends StatefulWidget {
+class SignUpScreen extends ConsumerStatefulWidget {
   const SignUpScreen({super.key});
 
   @override
-  State<SignUpScreen> createState() => _SignUpScreenState();
+  ConsumerState<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _SignUpScreenState extends State<SignUpScreen> {
+class _SignUpScreenState extends ConsumerState<SignUpScreen> {
   late final _emailController;
   late final _nameController;
   late final _passwordController;
   late final _cnfpswdController;
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -45,159 +46,201 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(35.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 20,
-            children: [
-              InkWell(
-                child: Icon(
-                  Icons.arrow_back,
-                  color: Theme.of(context).colorScheme.secondary.withAlpha(150),
-                ),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-
-              Text("Sign Up", style: TextStyle(fontSize: 35)),
-
-              TextField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  hintText: 'Name',
-                  hintStyle: TextStyle(
+      body: Form(
+        key: _formKey,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(35.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 20,
+              children: [
+                InkWell(
+                  child: Icon(
+                    Icons.arrow_back,
                     color: Theme.of(
                       context,
-                    ).colorScheme.secondary.withAlpha(200),
+                    ).colorScheme.secondary.withAlpha(150),
                   ),
-                  fillColor: Theme.of(
-                    context,
-                  ).colorScheme.secondary.withAlpha(50),
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide.none,
-                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
                 ),
-              ),
-              TextField(
-                controller: _emailController,
 
-                decoration: InputDecoration(
-                  hintText: 'Email',
-                  hintStyle: TextStyle(
-                    color: Theme.of(
+                Text("Sign Up", style: TextStyle(fontSize: 35)),
+
+                TextFormField(
+                  controller: _nameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Name',
+
+                    hintStyle: TextStyle(
+                      color: Theme.of(
+                        context,
+                      ).colorScheme.secondary.withAlpha(200),
+                    ),
+
+                    fillColor: Theme.of(
                       context,
-                    ).colorScheme.secondary.withAlpha(200),
-                  ),
-                  fillColor: Theme.of(
-                    context,
-                  ).colorScheme.secondary.withAlpha(50),
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  hintStyle: TextStyle(
-                    color: Theme.of(
-                      context,
-                    ).colorScheme.secondary.withAlpha(200),
-                  ),
-                  fillColor: Theme.of(
-                    context,
-                  ).colorScheme.secondary.withAlpha(50),
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              TextField(
-                controller: _cnfpswdController,
-                decoration: InputDecoration(
-                  hintText: 'Confirm Password',
-                  hintStyle: TextStyle(
-                    color: Theme.of(
-                      context,
-                    ).colorScheme.secondary.withAlpha(200),
-                  ),
-                  fillColor: Theme.of(
-                    context,
-                  ).colorScheme.secondary.withAlpha(50),
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 5),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        log('email ${_emailController.text}');
-                        log('password ${_passwordController.text}');
-
-                        await AuthService()
-                            .signingInUser(
-                              user: UserInfoModel(
-                                mailId: _emailController.text,
-                                userName: _nameController.text,
-                              ),
-                              password: _passwordController.text,
-                            )
-                            .then((final val) {
-                              log('Saved in firebase and logged in..');
-                            });
-                        // try {
-                        //   final credential = await FirebaseAuth.instance
-                        //       .createUserWithEmailAndPassword(
-                        //         email: _emailController.text,
-                        //         password: _passwordController.text,
-                        //       );
-                        //   log('password and user id created successfully');
-                        // } on FirebaseAuthException catch (e) {
-                        //   if (e.code == 'weak-password') {
-                        //     log(' The password provided is too weak ');
-                        //   } else if (e.code == 'email-already-in-use') {
-                        //     log('The account already exists in mail');
-                        //   }
-                        // } catch (e) {
-                        //   log('error from firebase ${e}');
-                        // }
-
-                        // Navigator.pushNamed(
-                        //   context,
-                        //   RouteNavigations.homeScreenWithoutSkip,
-                        // );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.cyan,
-                        foregroundColor: Colors.black,
-                        padding: EdgeInsets.all(20),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      child: Text('Submit'),
+                    ).colorScheme.secondary.withAlpha(50),
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter your email to proceed';
+                    } else {
+                      final emailRegex = RegExp(
+                        r'^[\w-\.]+@([\w-]+\.+[\w-]{2,4}$)',
+                      );
+
+                      if (!emailRegex.hasMatch(value)) {
+                        return 'Enter valid mail';
+                      }
+                    }
+
+                    return null;
+                  },
+
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    hintStyle: TextStyle(
+                      color: Theme.of(
+                        context,
+                      ).colorScheme.secondary.withAlpha(200),
+                    ),
+                    fillColor: Theme.of(
+                      context,
+                    ).colorScheme.secondary.withAlpha(50),
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password cannot be empty';
+                    } else {
+                      final pswdRegEx = RegExp(
+                        r'^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{3,}$',
+                      );
+
+                      if (!pswdRegEx.hasMatch(value)) {
+                        return 'Enter valid password';
+                      }
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      color: Theme.of(
+                        context,
+                      ).colorScheme.secondary.withAlpha(200),
+                    ),
+                    fillColor: Theme.of(
+                      context,
+                    ).colorScheme.secondary.withAlpha(50),
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  controller: _cnfpswdController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter confirm password';
+                    } else {
+                      if (value != _passwordController.text) {
+                        return 'Mismatch in password and Confirm password';
+                      }
+
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Confirm Password',
+                    hintStyle: TextStyle(
+                      color: Theme.of(
+                        context,
+                      ).colorScheme.secondary.withAlpha(200),
+                    ),
+                    fillColor: Theme.of(
+                      context,
+                    ).colorScheme.secondary.withAlpha(50),
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 5),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            log('current state validated');
+                            await AuthService()
+                                .signingInUser(
+                                  user: UserInfoModel(
+                                    mailId: _emailController.text,
+                                    userName: _nameController.text,
+                                  ),
+                                  password: _passwordController.text,
+                                  ref: ref,
+                                )
+                                .then((final val) {
+                                  log('Saved in firebase and logged in..');
+                                  if (!context.mounted) return;
+                                  Navigator.pushNamed(
+                                    context,
+                                    RouteNavigations.homeScreenWithoutSkip,
+                                  );
+                                });
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(content: Text('Failed to save')),
+                            );
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.cyan,
+                          foregroundColor: Colors.black,
+                          padding: EdgeInsets.all(20),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Text('Submit'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
