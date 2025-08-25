e72c06326e66babee09ef0ba291d8764f9a01a61
priya31-prog <karthika200103@gmail.com>
2025-08-25 14:27:48 +0530

    profile notifier

---
+++ basic_ecommerce_app/lib/screens/login_profile/profile_notifier.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:basic_ecommerce_app/common_files/shared_preference/shared_preferences_call.dart';
+import 'package:basic_ecommerce_app/screens/login_profile/user_info_model.dart';
+import 'package:basic_ecommerce_app/state_management/notifiers.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProfileNotifier extends AsyncNotifier<UserInfoModel> {
+  @override
+  FutureOr<UserInfoModel> build() async {
+    final user = ref.watch(userId); // create one and call here
+    return await fetchUser(userId: user);
+  }
+
+  Future<UserInfoModel> fetchUser({required String userId}) async {
+    final docRef = FirebaseFirestore.instance
+        .collection('user_information')
+        .doc(userId);
+
+    final snapShot = await docRef.get();
+
+    final userDataJson = snapShot.data();
+
+    log('user data json from profile notifier -- ${userDataJson}');
+
+    final userJson = UserInfoModel.fromJson(userDataJson ?? {});
+    CacheData.instance.setUserLoggedIn('isUserLoggedIn', true);
+    state = AsyncData(userJson);
+    return userJson;
+  }
+}
+
+final profileProvider = AsyncNotifierProvider<ProfileNotifier, UserInfoModel>(
+  ProfileNotifier.new,
+);
