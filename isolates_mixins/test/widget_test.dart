4a5e5e4559096f91fbfb740cf287ef8f1b13d504
priya31-prog <12345678+username@users.noreply.github.com>
2025-09-27 12:26:07 +0530

    success test case

--- isolates_mixins/test/widget_test.dart
+++ isolates_mixins/test/widget_test.dart
@@ -7,24 +7,35 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:isolates_mixins/main.dart';
+import 'package:isolates_mixins/common_files/route_navigation.dart';
+import 'package:isolates_mixins/home_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Home page with mixin and isolates', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(
+      MaterialApp(
+        routes: {
+          RouteNavigation.mixinPage:
+              (context) => Scaffold(body: Text('Mixin Page')),
+        },
+        home: const MyHomePage(),
+      ),
+    );
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Mixins'), findsOneWidget);
+    expect(find.text('Isolates'), findsOneWidget);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.tap(find.text('Mixins'));
+    await tester.pumpAndSettle();
+
+    // await tester.tap(find.text('Isolates'));
+    // await tester.pumpAndSettle();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Mixin Page'), findsOneWidget);
+    // expect(find.text('1'), findsOneWidget);
   });
 }
